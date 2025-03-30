5e28c688c077c60330dff6558ee9974dcfa99ed8
harry chow duarte <chow@debian.com>
2025-03-29 19:51:07 -0600

    subir guia

--- src/modules/cars/services/cars.service.ts
+++ src/modules/cars/services/cars.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CreateCarDto } from '../dto/car.dto';
+import { Repository } from 'typeorm';
+import { Car } from '../entities/car.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class CarsService {}
+export class CarsService {
+  constructor(
+    @InjectRepository(Car)
+    private readonly carRepository: Repository<Car>,
+  ) {}
+
+  async create(createCarDto: CreateCarDto) {
+    try {
+      console.log('DTO recibido:', createCarDto);
+      const car = this.carRepository.create(createCarDto);
+      await this.carRepository.save(car);
+
+      return car;
+    } catch (error) {
+      console.log('Error al guardar el vehículo:', error);
+      throw new InternalServerErrorException('Ayuda!');
+    }
+  }
+}
